0a6b9a870b52a7026672d3cbfa3030caf2a56531
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-07-22 23:12:01 +0200

    add data in seed , show details

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,18 +1,40 @@
 const express = require("express")
 const router = express.Router()
-
+const Event = require('../models/Event')
 
 // route to home events
-router.get('/', (req,res)=> {
-    
-    res.render('event/index')
+router.get('/', (req,res)=> {   
+    Event.find({}, (err,events)=> {
+   //     res.json(events)
+        let chunk = []
+        let chunkSize = 3
+        for (let i =0 ; i < events.length ; i+=chunkSize) {
+            chunk.push(events.slice( i, chunkSize + i))
+        }
+        //res.json(chunk)
+         res.render('event/index', {
+             chunk : chunk
+         })
+    })
+  
 })
 
 // show single event
-
 router.get('/:id', (req,res)=> {
+    Event.findOne({_id: req.params.id}, (err,event)=> {
+        
+       if(!err) {
+           
+        res.render('event/show', {
+            ali: event
+        })
+
+       } else {
+           console.log(err)
+       }
     
-    res.render('event/show')
+    })
+ 
 })
 
 
