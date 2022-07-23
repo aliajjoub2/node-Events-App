8c61aea8a018f75c1a212e76c946a68913138e3d
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-07-23 23:08:48 +0200

    flash. session , validation, add to database

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require("express")
 const app = express()
-// bring ejs template
-
-app.set('view engine', 'ejs')
-
-
-
+const bodyParser = require('body-parser')
 // add database.js
 const db = require('./config/database')
+const session = require('express-session')
+const flash = require('connect-flash')
+
+
+// bring ejs template
+app.set('view engine', 'ejs')
+
+// bring body parser 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 //bring static
 
 app.use(express.static('public'))
 app.use(express.static('node_modules'))
+// session and flash config .
+app.use(session({
+    secret: 'lorem ipsum',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 60000 * 15}
+}))
+app.use(flash())
 
   
 app.get('/', (req,res)=> {
--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const Event = require('../models/Event')
+// add validate paqage
+const { check, validationResult } = require('express-validator/check')
 
 // route to home events
 router.get('/', (req,res)=> {   
@@ -13,10 +15,57 @@ router.get('/', (req,res)=> {
         }
         //res.json(chunk)
          res.render('event/index', {
-             chunk : chunk
+             chunk : chunk,
+             message: req.flash('info')
          })
     })
   
+})
+
+//create new events
+
+router.get('/create', (req,res)=> {
+
+    res.render('event/create', {
+        errors: req.flash('errors')
+    })
+})
+// save event to db
+
+router.post('/create', [
+    check('title').isLength({min: 5}).withMessage('Title should be more than 5 char'),
+    check('description').isLength({min: 5}).withMessage('Description should be more than 5 char'),
+    check('location').isLength({min: 3}).withMessage('Location should be more than 5 char'),
+    check('date').isLength({min: 5}).withMessage('Date should valid Date'),
+
+] , (req,res)=> {
+
+    const errors = validationResult(req)
+
+    if( !errors.isEmpty()) {
+        req.flash('errors',errors.array())
+        res.redirect('/events/create')
+    } else {
+        
+        let newEvent = new Event({
+            title: req.body.title,
+            description: req.body.description,
+            date: req.body.date,
+            location: req.body.location,
+            created_at: Date.now()
+        })
+
+        newEvent.save( (err)=> {
+            if(!err) {
+                console.log('event was added')
+                req.flash('info', ' The event was created successfuly')
+                res.redirect('/events')
+            } else {
+                console.log(err)
+            }
+        })
+    }
+   
 })
 
 // show single event
